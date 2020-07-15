8d2f06f858ef8ac5fba38a2b28ddd6d0c8db8d1b
aakashdeveloper <ahanda205@gmail.com>
2020-07-15 21:41:10 +0530

    A

--- routing/src/component/Hooks.js
+++ routing/src/component/Hooks.js
@@ -1,9 +1,24 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
+import DisplayNews from './DisplayNews';
 
 function HooksComponent(){
     const [title] = useState('DeveloperFunnel');
     const [subheading] = useState('React Hooks');
     const [counter,setCount] = useState(0);
+    const [news,setNews] = useState()
+    const [dummy] = useState()
+
+    const url = "http://localhost:8900/news"
+
+    console.log("Outside")
+    useEffect(() => {
+        console.log("Inside")
+        fetch(url)
+        .then(res =>res.json())
+        .then((data) => {
+            setNews(data)
+        })
+    },[dummy])
 
     return(
         <div>
@@ -13,6 +28,7 @@ function HooksComponent(){
             <button onClick={() => {setCount(counter+1)}}>
                 Counter
             </button>
+            <DisplayNews mydata={news}/>
         </div>
     )
 }
